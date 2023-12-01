cade7d1e6cc5af24d370513ec4497c0733dc48d6
Selene Choi <selenechoi0214@gmail.com>
2023-11-30 20:50:06 -0800

    Folder Structure, Tailwindcss created

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
