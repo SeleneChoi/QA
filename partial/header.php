d878631d26e76508c9e33cd4b7757549c3cdd60e
Jpduque25 <jpduque25@gmail.com>
2023-12-07 13:58:14 -0800

    inserted header in partial folder

---
+++ partial/header.php
@@ -0,0 +1,32 @@
+<header>
+        <nav class = "grid grid-cols-12 gap-8 px-5">
+            <div class = "lg:col-start-1 lg:col-span-2">
+                <a href="homepg.html"><img src="" alt="logo"></a>
+            </div>
+
+            <div class = "hidden lg:block lg:col-start-7 lg:col-span-6">
+
+                <div class = "hidden sm:block">
+                    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                        <i class="fa fa-bars"></i>
+                      </a>
+                </div>
+
+                <div class="burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <ul>
+                    <li><button class="navButton">Join Event</button></li>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="EventDetails.html">Event Details</a></li>
+                    <li><a href="OurSpeakers.php">Our Speakers</a></li>
+                    <li><a href="#">Our Team</a></li>
+                </ul>
+                  
+            </div>
+        </nav>
+</header>
