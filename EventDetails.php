850ce22c85cf5b7e9e2694c32e10a4fdce536535
Jpduque25 <jpduque25@gmail.com>
2023-12-07 13:44:53 -0800

    Updated event and index pages.

---
+++ EventDetails.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Details</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="css\style.css" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+    <?php include'partial/header.php'?>
+
+    <main>
+        
+        <div class="content">
+
+                
+            <div class = "container mx-auto w-full max-w-full">
+
+                <div class="purple1 py-20">
+                    <div class="flex justify-center items-center">
+                            <h1 class = "text-3xl orange">Event Schedule</h1>
+                    </div>
+                </div>
+
+
+                <div class="purple2 py-5 lg:py-14 px-5 lg:px-8">
+
+                    <div class="grid grid-cols-2 lg:grid-cols-12 w-full mb-10 gap-8 ">
+                        <div class= "col-span-full lg:col-start-2 lg:col-span-2">
+                            <h2 class = "text-lg lg:text-2xl orange">3:30 PM</h2>
+                        </div>
+
+                        <div class = "col-span-full lg:col-start-5 lg:col-span-7">
+                            <li class = "text-sm lg:text-lg">Event opens up for students. students receive event materials including Networking Bingo.</li>
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="grid grid-cols-2 lg:grid-cols-12 w-full mb-10 gap-8">
+                        
+                        <div class="col-span-full lg:col-start-2 lg:col-span-2">
+                            <h2 class = "text-lg lg:text-2xl orange">3:30 PM - 4:30 PM</h2>
+                        </div>
+                        
+                        <div class="col-span-full lg:col-start-5 lg:col-span-7">
+                            <li class = "text-sm lg:text-lg mb-5">New Media Alumni Open Showcase, students interact with Alumni’s of different career paths. </li>
+                            <li class = "text-sm lg:text-lg mb-5">Professional Headshot Photo booth open.</li>
+                            <li class = "text-sm lg:text-lg mb-5">Tech Collider digital Mural open, use QR code to leave your mark.</li>
+
+
+                        </div>
+                      
+                    </div>
+
+                    <div class="grid grid-cols-2 lg:grid-cols-12 w-full mb-10 gap-8">
+                        
+                        <div class = "col-span-full lg:col-start-2 lg:col-span-2">
+                            <h2 class = "text-lg lg:text-2xl orange">4:30 PM - 5:30 PM</h2>
+                        </div>
+
+                        <div class = "col-span-full lg:col-start-5 lg:col-span-7">
+                            <li class = "text-sm lg:text-lg mb-5">First industry speaker presentation (20-30 mins).</li>
+                            <li class = "text-sm lg:text-lg mb-5">Second industry speaker presentation (20-30 mins).</li>
+                        </div>
+                      
+                    </div>
+
+
+                    <div class="grid grid-cols-2 lg:grid-cols-12 w-full mb-10 gap-8">
+
+                        <div class = "col-span-full lg:col-start-2 lg:col-span-2">
+                            <h2 class = "text-lg lg:text-2xl orange">5:30 PM - 6:30 PM</h2>
+                        </div>
+
+                        <div class = "col-span-full lg:col-start-5 lg:col-span-7">
+                            <li class = "text-sm lg:text-lg mb-5">Food and refreshments served.</li>
+                            <li class = "text-sm lg:text-lg mb-5">Last call for Professional Headshot @ 6:30 pm.</li>
+                            <li class = "text-sm lg:text-lg mb-5">Collect completed bingos for raffle prize.</li>
+                        </div>
+                
+                    </div>
+
+
+                    <div class="grid grid-cols-2 lg:grid-cols-12 w-full  gap-8">
+
+                        <div class = "col-span-full lg:col-start-2 lg:col-span-2">
+                            <h2 class = "text-lg lg:text-2xl orange">6:30 PM - 7:00 PM</h2>
+                        </div>
+
+                        <div class = "col-span-full lg:col-start-5 lg:col-span-7">
+                            <li class = "text-sm lg:text-lg mb-5">Announce winners of activities and distribute prizes.</li>
+                            <li class = "text-sm lg:text-lg mb-5">Closing ceremony, thank you speech from event team.</li>
+
+                        </div>
+                     
+                    </div>
+
+                </div>
+               
+                <div> 
+
+                    <div class="purple1 grid grid-cols-2 lg:grid-cols-12 w-full gap-8 py-5 lg:py-14 px-5 lg:px-8 justify-center items-center">
+                        
+                        <div class="col-span-full lg:col-start-2 lg:col-span-5 ">
+                            <h2 class ="text-lg lg:text-2xl orange mb-5 lg:mb-10">Network Bingo</h2>
+
+                            <p class = "text-sm lg:text-lg leading-8">Engage with NM Alumni’s and complete your Network Bingo!  Fill your bingo card with thrilling networking challenges like discovering someone from a different industry or connecting with a professional who shares your passions. Once you complete it, submit it to get a chance at winning amazing prizes at the end of the event!</p>
+                        </div>
+
+                        <div class="col-span-full lg:col-start-7 lg:col-span-5">
+                            <img src="images/eventdetailsimg1.png" alt="">
+                        </div>
+                    </div>
+
+
+
+                    <div class="purple2 grid grid-cols-2 lg:grid-cols-12 w-full  gap-8 py-5 lg:py-14 px-5 lg:px-8 justify-center items-center">                            
+                        <div class="col-span-full lg:col-start-2 lg:col-span-5">
+                            <img src="images/eventdetailsimg2.png" alt="">
+                        </div>
+
+                        <div class="col-span-full lg:col-start-7 lg:col-span-5">
+                            <h2 class = "text-lg lg:text-2xl orange mb-5 lg:mb-10">Headshot Photobooth</h2>
+                            <p class = "text-sm lg:text-lg leading-8">Upgrade your professional image effortlessly with our Headshot Photobooth.  Capture your best side, helping you present a polished and authentic representation of yourself for Linkedin profile or portfolio website!</p>
+                        </div>
+                    </div>
+
+                    
+
+                    <div class="purple1 grid grid-cols-2 lg:grid-cols-12 w-full  gap-8 py-5 lg:py-14 px-5 lg:px-8 justify-center items-center">  
+                        <div class=" col-span-full lg:col-start-2 lg:col-span-5">
+                            <h2 class = "text-lg lg:text-2xl orange mb-5 lg:mb-10">Digital Mural</h2>
+                            <p class = "text-sm lg:text-lg leading-8">Leave your mark at Media Block with our Live Digital Mural, capturing the collaborative spirit of the New Media community. Watch as the mural comes to life in real-time, symbolizing the collective support, confidence, and excitement we aim to infuse into your career journey. As a special touch, we'll send all participants a copy of the mural at the end of the event.</p>
+                        </div>
+
+                        <div class="col-span-full lg:col-start-7 lg:col-span-5">
+                            <img src="images/eventdetailsimg3.png" alt="">
+                        </div>
+                    </div>
+
+
+                    <div class="purple2 grid grid-cols-2 lg:grid-cols-12 w-full  gap-8 py-5 lg:py-14 px-5 lg:px-8 justify-center items-center">                         
+                        <div class=" col-span-full lg:col-start-2 lg:col-span-5">
+                            <img src="images/eventdetailsimg4.png" alt="">
+                        </div>
+
+                        <div class="col-span-full lg:col-start-7 lg:col-span-5">
+                            <h2 class = "text-lg lg:text-2xl orange mb-5 lg:mb-10">Snack and Refreshments</h2>
+                            <p class = "text-sm lg:text-lg leading-8">What’s an event without some food? Media Block will provide some tasty treats and drinks for all participants! 
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+        
+
+            </div>
+
+        </div>
+
+    </main>
+
+    <?php include'partial/footer.php'?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="css\style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <?php include'partial/header.php'?>
+
+
+    <main>
+        <div class="mx-auto w-full max-w-full">
+            <div class = "purple1 grid grid-cols-2 lg:grid-cols-12 justify-center items-center w-full gap-8 py-10 lg:py-20  px-5">
+                <div class = "col-span-full lg:col-start-2 lg:col-span-5">
+                
+                    <h1 class = " lg:pb-10 pb-4">
+                        <span class = "text-xl lg:text-5xl beige">Take a </span> 
+                        <span class="text-xl lg:text-5xl orange">Media Brick :D</span>
+                    </h1>
+
+                    <div>
+                        <p class = "text-sm lg:text-lg leading-8">Join our event, where New Media students connect, network, and shape their future alongside alumni and industry experts!</p>
+
+                        <div class = "pt-5 text-sm lg:text-lg">
+                            <i>BCIT Downtown Campus</i>
+                            <br>
+                            <i>February 20,2024</i>
+                            <br>
+                            <button class = "button-main mt-7 w-4/4  ">Join Event</button>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <div class="col-span-full lg:col-start-7 lg:col-span-5">
+                    <img src="images/homepgimg1.png" class="w-4/4" alt="">
+                </div>
+            </div>
+
+
+            <div class="purple2 grid grid-cols-2 lg:grid-cols-12 justify-center items-center w-full gap-8 py-10 lg:py-14  px-5">
+                <div class="col-span-full lg:col-start-2 lg:col-span-5">
+                    <img src="images/homepgimg2.png" alt="">
+                </div>
+
+                <div class="col-span-full lg:col-start-7 lg:col-span-5">
+                    <h2 class = "text-lg lg:text-2xl orange mb-5 lg:mb-5">Vision</h2>
+                    <p class = "text-sm lg:text-lg leading-8 font-light">What would life after New Media look like? Where could I, a student of New Media, be after I graduate? Imagine students being able to meet their “future-selves” by bringing former alumni’s of New Media together. The event would bridge the gap between current students and industry experts who once were in their shoes. To connect, network, and build the New Media community beyond the downtown campus.</p>
+                </div>
+            </div>
+
+
+            <div class="purple1 grid grid-cols-2 lg:grid-cols-12 justify-center items-center w-full gap-8 py-10 lg:py-14  px-5">
+                <div class="col-span-full lg:col-start-2 lg:col-span-5">
+                    <h2 class = "text-lg lg:text-2xl orange mb-5 lg:mb-5">Mission</h2>
+                    <p class = "text-sm lg:text-lg leading-8 font-light">Since the end of the pandemic, the scarcity of opportunities is increasingly disheartening for current New Media students. Juggling the program full-time courses, endless portfolio preparation, the pursuit of opportunities beyond their academic commitments has proven to be overwhelming. We hope to bring the opportunities to the students in hopes to give support, confidence, and excitement in their career journeys.</p>
+                </div>
+
+                <div class="col-span-full lg:col-start-7 lg:col-span-5">
+                    <img src="images/homepgimg3.png" alt="">
+                </div>
+                
+            </div>
+                    
+        </div>
+
+    </main>
+
+    <?php include'partial/footer.php'?>
+
+
+</body>
+</html>
