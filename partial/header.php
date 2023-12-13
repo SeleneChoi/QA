ea683e8264b607e2582ab58aab7c727eb4c1982f
Jpduque25 <jpduque25@gmail.com>
2023-12-12 22:32:58 -0800

    navigation menu fix

--- partial/header.php
+++ partial/header.php
@@ -1,32 +1,34 @@
+
 <header>
-        <nav class = "grid grid-cols-12 gap-8 px-5">
-            <div class = "lg:col-start-1 lg:col-span-2">
-                <a href="homepg.html"><img src="" alt="logo"></a>
-            </div>
+    <nav class="main-nav">
+        <div class="logo-container">
+            <a href="index.php"><img class="logo" src="images/logo.png" alt="logo"></a>
+        </div>
 
-            <div class = "hidden lg:block lg:col-start-7 lg:col-span-6">
+        <div class="hamburger" onclick="toggleMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
 
-                <div class = "hidden sm:block">
-                    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                        <i class="fa fa-bars"></i>
-                      </a>
-                </div>
+        <div class="menu-container">
+            <ul class="menu">
+                <li class="navButton"><a href="#registration">Join Event</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="EventDetails.php">Event Details</a></li>
+                <li><a href="OurSpeakers.php">Our Speakers</a></li>
+                <li><a href="OurTeam.php">Our Team</a></li>
+            </ul>
+        </div>
 
-                <div class="burger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-
-                <ul>
-                    <li><a href="#registration"><button class="navButton">Join Event</button></a></li>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="EventDetails.php">Event Details</a></li>
-                    <li><a href="OurSpeakers.php">Our Speakers</a></li>
-                    <li><a href="OurTeam.php">Our Team</a></li>
-                </ul>
-                  
-            </div>
-        </nav>
+    </nav>
 </header>
+
+
+<script>
+    function toggleMenu() {
+    var menuContainer = document.querySelector('.menu-container');
+    menuContainer.classList.toggle('show');
+}
+
+</script>
